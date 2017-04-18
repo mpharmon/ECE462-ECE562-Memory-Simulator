f155d6ee5aebe967be56a9cbc93bd322c2230dae
Mike Harmon <mpharmon@email.arizona.edu>
2017-04-17 19:53:15 -0700

    Updated for new Memory Constructor
    Memory no longer requires a block size.

--- MemorySimulator/src/edu/arizona/ece/memsim/implementations/stream/StreamProgram.java
+++ MemorySimulator/src/edu/arizona/ece/memsim/implementations/stream/StreamProgram.java
@@ -1,7 +1,5 @@
 package edu.arizona.ece.memsim.implementations.stream;
 
-import java.util.Random;
-
 import edu.arizona.ece.memsim.implementations.core.Program;
 import edu.arizona.ece.memsim.model.Memory;
 
@@ -12,9 +10,8 @@ public class StreamProgram extends Program {
 	}
 	
 	protected static void Reset() throws Exception{
-		// Currently Block Sizes Must Be EQUAL Among all Cache Level(s) and Memory
 		mem = null;
-		mem = new Memory(134217728, 64, 200);// 128MB, 64B Block, 200 Cycle Access
+		mem = new Memory(134217728, 200);// 128MB, 64B Block, 200 Cycle Access
 		L1 = null;
 		L1 = new StreamPrefetcherCacheController(1, 8192, 64, 0, 1, mem);// 8KB, 64B Block, Fully Associative, 1 Cycle Access
 	}
